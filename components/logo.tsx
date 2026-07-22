337d00eae388c998d8a4434cec930a3bd926c801
jayjay2904 <john_barnett@mail.com>
2026-07-22 12:34:30 +0100

    Promote refreshed blue brand mark

--- components/logo.tsx
+++ components/logo.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export function Logo({ compact = false }: { compact?: boolean }) {
   return <Link className="logo" href="/" aria-label="One Better Labs home">
-    <Image src="/brand/ob-mark.svg" width={48} height={38} alt="" priority />
+    <Image src="/brand/ob-mark-blue.png" width={144} height={110} alt="" priority />
     {!compact && <span className="logo-text">One Better <b>Labs</b></span>}
   </Link>;
 }
